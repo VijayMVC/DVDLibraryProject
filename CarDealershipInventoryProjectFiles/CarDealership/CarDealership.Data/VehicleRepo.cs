ad652913566b6662b9588cc7c4db214d87a2f111
Paul Bargerstock <paul.bargerstock84@gmail.com>
2015-10-29 11:53:54 -0400

    added connection settings & started vehicle repo

---
+++ CarDealershipInventoryProjectFiles/CarDealership/CarDealership.Data/VehicleRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarDealership.Data.Config;
+using CarDealership.Models;
+using Dapper;
+
+namespace CarDealership.Data
+{
+    public class VehicleRepo
+    {
+        public List<Vehicle> GetAll()
+        {
+            var results = new List<Vehicle>();
+            using (SqlConnection cn = new SqlConnection(Settings.ConnectionString))
+            {
+                results = cn.Query<Vehicle>("SELECT * FROM Vehicle").ToList();
+            }
+            return results;
+        }
+    }
+}
